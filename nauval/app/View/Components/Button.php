1ebe6bd01bae9d9a8c355314fb199863d92f1937
Nauval-Alghiffary <nauval.gif@gmail.com>
2021-04-26 16:22:19 +0700

    instalasi laravel dan adminlte

---
+++ nauval/app/View/Components/Button.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\View\Components;
+
+use Illuminate\View\Component;
+
+class Button extends Component
+{
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    
+    public $label, $theme, $icon;
+    public function __construct($label, $theme, $icon=null)
+    {
+        $this->label = $label;
+        $this->theme = $theme;
+        $this->icon = $icon;
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|\Closure|string
+     */
+    public function render()
+    {
+        return view('components.button');
+    }
+}
