b2aed3dd82e4cab0db206a7bb701300f74ec6231
Nauval-Alghiffary <nauval.gif@gmail.com>
2021-04-29 00:16:59 +0700

    crud di kategori dan merek(masih error)

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\BrandsController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CategoriesController;
+use GuzzleHttp\Middleware;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,8 +29,7 @@ Route::get('/admin/home', [App\Http\Controllers\AdminController::class, 'index']
     ->name('admin.home')
     ->middleware('is_admin');
 
-Route::get('admin/kelola_barang', 
-[App\Http\Controllers\ProductController::class, 'index'])
+Route::get('admin/kelola_barang', [App\Http\Controllers\ProductController::class, 'index'])
     ->name('admin.product')
     ->middleware('is_admin');
 
@@ -45,6 +52,57 @@ Route::get('admin/ajaxadmin/dataBuku/{id}', [barangController::class, 'getDataBu
 Route::delete('admin/barangs/delete', [barangController::class, 'destroy'])
     ->name('admin.barang.delete')
     ->middleware('is_admin');
+
+//Route Categories
+Route::get('admin/kategori', [CategoriesController::class, 'index'])
+    ->name('admin.kategori')
+    ->middleware('is_admin');
+Route::post('admin/kategori', [CategoriesController::class, 'add_categories'])
+    ->name('admin.kategori.submit')
+    ->middleware('is_admin');
+//route edit categories
+Route::patch('admin/kategori/update', [CategoriesController::class, 'update_categories'])
+    ->name('admin.kategori.update')
+    ->middleware('is_admin');
+Route::get('admin/ajaxadmin/dataCategories/{id}', [CategoriesController::class, 'getDataCategories']);
+
+//route delete categories
+Route::delete('admin/kategori/delete', [CategoriesController::class, 'delete_categories'])
+    ->name('admin.kategori.delete')
+    ->middleware('is_admin');
+
+
+//ROUTE UTAMA BRANDS
+Route::get('admin/merek', [App\Http\Controllers\BrandsController::class, 'index'])
+    ->name('admin.merek')
+    ->middleware('is_admin');
+
+//route tambah brands
+Route::post('admin/merek', [BrandsController::class, 'add_brand'])
+    ->name('admin.brand.submit')
+    ->middleware('is_admin');
+
+//route edit brands
+Route::patch('admin/merek/update', [BrandsController::class, 'update_brands'])
+    ->name('admin.brand.update')
+    ->middleware('is_admin');
+Route::get('admin/ajaxadmin/dataBrands/{id}', [BrandsController::class, 'getDataBrands']);
+
+//route delete brands
+Route::delete('admin/merek/delete', [BrandsController::class, 'delete_brands'])
+    ->name('admin.brand.delete')
+    ->middleware('is_admin');
+
+
+//Route Product
+Route::get('admin/kelola_barang', [App\Http\Controllers\ProductController::class, 'index'])
+    ->name('admin.product')
+    ->middleware('is_admin');
+
+Route::post('admin/kelola_barang', [ProductController::class, 'add_product'])
+    ->name('admin.product.submit')
+    ->middleware('is_admin');
+
 
 Auth::routes();
 
