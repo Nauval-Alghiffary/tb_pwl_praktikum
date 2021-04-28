64dac7b2904305a2b24f7287a8f9d1d7ea34dd39
Nauval-Alghiffary <nauval.gif@gmail.com>
2021-04-28 22:38:55 +0700

    tambah tampilan menu barang

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,30 @@ Route::get('/admin/home', [App\Http\Controllers\AdminController::class, 'index']
     ->name('admin.home')
     ->middleware('is_admin');
 
+Route::get('admin/kelola_barang', 
+[App\Http\Controllers\ProductController::class, 'index'])
+    ->name('admin.product')
+    ->middleware('is_admin');
+
+Route::post('admin/kelola_barang', [ProductController::class, 'add_product'])
+    ->name('admin.product.submit')
+    ->middleware('is_admin');
+
+Route::get('admin/barangs', [barangController::class, 'index'])
+    ->name('admin.barangs')
+    ->middleware('is_admin');
+
+Route::post('admin/barangs', [barangController::class, 'store'])
+    ->name('admin.barang.submit')
+    ->middleware('is_admin');
+Route::patch('admin/barangs/update', [barangController::class, 'update'])
+    ->name('admin.barang.update')
+    ->middleware('is_admin');
+
+Route::get('admin/ajaxadmin/dataBuku/{id}', [barangController::class, 'getDataBuku']);
+Route::delete('admin/barangs/delete', [barangController::class, 'destroy'])
+    ->name('admin.barang.delete')
+    ->middleware('is_admin');
 
 Auth::routes();
 
